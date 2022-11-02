a0c7ecba12f65d6607ca4cdf640b535212986f02
Somil Swapnil Chandra <46962797+Alankriss07@users.noreply.github.com>
2022-11-02 18:08:15 +0530

    upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+
+
+		
+if __name__ == '__main__':
+   app.run(debug = True)
